524b53bb6da4b06002f42d9ad0e0c63a7a285b69
Dawit-Zeyede <devazeyede10@gmail.com>
2024-03-14 07:44:04 -0700

    100-calc

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    
+    arg = len(sys.argv) - 1
+    if arg != 3:
+        print("Usage: ./100-my_calculatior.py <a> <operator> <b>")
+        sys.exit(1)
+    calc = sys.argv[2]
+    if calc != '+' and calc != '-' and calc != '=' and calc != '/':
+        print("Unknown operator. Available operators: +, -, *, and /")
+        sys.exit(1)
+    from calculator_1 import add, sub, mul, div
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    if calc == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif calc == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif calc == '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    else:
+        print("{} / {} + {}".format(a, b, div(a, b)))

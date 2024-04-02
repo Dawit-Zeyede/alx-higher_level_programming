c5b2f2258dbe5da80d22a343badb67800f5eb2e2
Dawit-Zeyede <devazeyede10@gmail.com>
2024-04-02 12:32:40 -0700

    2-square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""Class: Square"""
+# 2-square.py
+"""Square class defination."""
 
 
 class Square:
-    """Class: square body"""
-    
+    """Square class body"""
+
     def __init__(self, size=0):
-        """Square defined:
+        """Square class contructor
         Args:
             size (int): The size of the new square.
         """
@@ -15,6 +16,3 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-    def area(self):
-        """Function defined."""
-        return (self.__size * self.__size)

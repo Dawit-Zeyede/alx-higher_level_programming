30663cee7f5b4a5584255ed6dbaea2516ef64384
Dawit-Zeyede <devazeyede10@gmail.com>
2024-04-08 17:56:23 -0700

    1-rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+Class: Rectangle
+"""
+
+
+class Rectangle:
+    """Class: Rectangel body. """
+
+    def __init__(self, width=0, height=0):
+        """Instance called and a new object is created."""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Calc Width
+        Args:
+            value: value of the width, must be a positive integer
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Calc height
+        Args:
+            value: value of the height, must be a positive integer
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value

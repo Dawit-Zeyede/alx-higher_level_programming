0bad69283ca6ed0ead7e73bf032e9f02091b2904
Dawit-Zeyede <devazeyede10@gmail.com>
2024-05-21 16:30:48 +0000

    INSERT

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
--- new value(row) to 'first_table' from 'hbtn_0c_0'
+-- insert new record  row in 'first_table' from db 'hbtn_0c_0'
 -- id == 89
 -- name = Best School
-INSERT INTO 'first_table' ('id', 'name') VALUES (89, "Best School");
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");

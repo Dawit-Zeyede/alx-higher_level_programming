39552eb6e30784881a1f3a06b084753f7b23f1af
Dawit-Zeyede <devazeyede10@gmail.com>
2024-05-22 18:57:35 +0000

    8-California

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- lists all cities of clifornia found in 'hbtn_0d_usa'
 SELECT id, name FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name = "California') ORDER BY id;
+WHERE state_id = (SELECT id FROM states WHERE name = "California") ORDER BY id;

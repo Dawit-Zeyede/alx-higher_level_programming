af0fff22e9e31f7bdc15c52fdb0858187c2b64c7
Dawit-Zeyede <devazeyede10@gmail.com>
2024-06-18 11:53:40 -0700

    for loop and argv

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,11 @@
-#!/bin/user/node
-if (isNaN(process.argv[2] || process.argv[2] === undefined)) {
-	console.log('Missing number of occurences');
+#!/usr/bin/node
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences');
 } else {
-	const x = Number(process.argv[2]);
-	let i = 0;
-	while (i < x) {
-		console.log('C is fun');
-		i++;
-	}
+  const x = Number(process.argv[2]);
+  let i = 0;
+  while (i < x) {
+    console.log('C is fun');
+    i++;
+  }
 }
